43769b7570cb46ae777bd92e8e0950b9f43bf1f6
Dragan Stankov <drashko1979@gmail.com>
2022-05-19 19:53:05 +0300

    bug fix

--- App/Config/database.php
+++ App/Config/database.php
@@ -3,12 +3,9 @@ return [
     'pdo' => [
           'driver' => 'mysql',
           'host' => 'localhost',
-          'db_name' => 'mvc_testing',//change it to 'mvc' in production
-         // 'db_name' => 'vgrifdrr_brightphp',//change it to 'mvc' in production
-         // 'db_user' => 'vgrifdrr_brightphp',//vgrifdrr_brightphp
-          'db_user' => 'root',//vgrifdrr_brightphp
-          'db_pass' => '',//^{U[f=+C.udO
-          //'db_pass' => '^{U[f=+C.udO',//^{U[f=+C.udO
+          'db_name' => '',//change it to 'mvc' in production
+          'db_user' => '',//
+          'db_pass' => '',
           'charset' => 'utf8',
           'default_fetch' => PDO::FETCH_OBJ
     ]
